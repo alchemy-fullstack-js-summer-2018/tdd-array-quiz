72429b80c1aeb6f7ddc5d2def13974af7052f554
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-09 16:22:39 -0700

    Add back old tests as comments and implement full functionality

--- test/oddSquare.test.js
+++ test/oddSquare.test.js
@@ -4,8 +4,18 @@ const oddSquare = require('../lib/oddSquare');
 describe('Array function', () => {
     let numbers = [2, 3, 9, 12, 8, 5, 7];
 
-    it('squares the odd numbers in an array', () => {
+    // it('returns only the odd numbers in an array', () => {
+    //     const result = oddSquare(numbers);
+    //     assert.deepEqual(result, [3, 9, 5, 7]);
+    // });
+
+    // it('squares the odd numbers in an array', () => {
+    //     const result = oddSquare(numbers);
+    //     assert.deepEqual(result, [9, 81, 25, 49]);
+    // });
+
+    it('only returns the squares of odd numbers that are greater than 24', () => {
         const result = oddSquare(numbers);
-        assert.deepEqual(result, [9, 81, 25, 49]);
+        assert.deepEqual(result, [81, 25, 49]);
     });
 });
