ab96877eb9941c130bb772fb144a26e438366ab9
MariahAdams <mariah.adams23@yahoo.com>
2018-07-09 16:27:55 -0700

    started second test

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -5,12 +5,13 @@ describe('array methods', () => {
 
     let numbers = [2, 3, 9, 12, 8, 5, 7];
 
-    it('returns an array', () => {
+    it.skip('returns an array', () => {
         const newNum = arrayMethod(numbers, n => n);
         assert.deepEqual(newNum, numbers);
     });
 
-    it.skip('returns a new array that contains the square of all the odd input, but only those squares that are greater than 24', () => {
-        
+    it('returns a new array that contains the square of all the odd input, but only those squares that are greater than 24', () => {
+        const expected = [81, 25, 49];
+        assert.equal(numbers, expected);
     });
 });
