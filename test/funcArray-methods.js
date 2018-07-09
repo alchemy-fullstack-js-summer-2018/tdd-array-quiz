c3899e048509f4ee3768d6d6fa344ad11e96128e
Carmen V Ramos <carmenvramos@yahoo.com>
2018-07-09 16:39:20 -0700

    Fixed array-method func

--- test/funcArray-methods.js
+++ test/funcArray-methods.js
@@ -5,6 +5,10 @@ describe('Square Odd numbers of Array', () => {
 
     it('SquareOdd', () => {
         const arr1 = [2, 3, 9, 12, 8, 5, 7];
-        const arr2 = [81, 25, 49];
-    })
-})
+        const arr2 = squareOdd(arr1, (item) => {
+            if (item % 2 === 1)
+            return  item * 2;
+    });
+    assert.deepEqual(arr2, [81, 25, 49]);
+
+});
