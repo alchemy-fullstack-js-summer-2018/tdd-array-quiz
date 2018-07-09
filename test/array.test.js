5b33b8b12b715a747d6f5e45818de2ae67489d93
Antreo Pukay <acrosswalker@yahoo.com>
2018-07-09 16:30:54 -0700

    test almost complete

---
+++ test/array.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const { oddNums } = require('../lib/array');
+
+describe('array method', () => {
+
+    describe('square of odd nums', () => {
+        it('squares the odd numbers in the array', () => {
+            const numbers = [2, 3, 9, 12, 8, 5, 7];
+            // const squared = numbers;
+            function oddNums(num) {
+                return num === (num % 2 === 0);
+                
+            }
+        });    
+    });
+});
