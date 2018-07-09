c1370a4c019949fc2c9bed29be1156c81e5f5caf
Carmen V Ramos <carmenvramos@yahoo.com>
2018-07-09 16:28:51 -0700

    Initial commit

---
+++ test/funcArray-methods.js
@@ -0,0 +1,10 @@
+const assert = (require('assert');
+
+
+describe('Square Odd numbers of Array', () => {
+
+    it('SquareOdd', () => {
+        const arr1 = [2, 3, 9, 12, 8, 5, 7];
+        const arr2 = [81, 25, 49];
+    })
+})
