dff2bc6ae9266a9ab693a65008f4ed110119d103
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-09 16:17:07 -0700

    Remove old test and add new for squaring

--- test/oddSquare.test.js
+++ test/oddSquare.test.js
@@ -2,9 +2,10 @@ const assert = require('assert');
 const oddSquare = require('../lib/oddSquare');
 
 describe('Array function', () => {
-    let numbers = [1, 2, 6, 9, 13];
-    it('gets only the odd numbers in an array', () => {
+    let numbers = [2, 3, 9, 12, 8, 5, 7];
+
+    it('squares the odd numbers in an array', () => {
         const result = oddSquare(numbers);
-        assert.deepEqual(result, [1, 9, 13]);
+        assert.deepEqual(result, [9, 81, 25, 49]);
     });
 });
