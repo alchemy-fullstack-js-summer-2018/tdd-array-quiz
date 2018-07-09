fd2bfb3eb11ec4d82ef2d14a0f15b91452c99c7a
Arthur Jen <arthurljen@gmail.com>
2018-07-09 16:17:30 -0700

    refactored tests

--- test/squarer.test.js
+++ test/squarer.test.js
@@ -4,12 +4,7 @@ const squarer = require('../lib/squarer');
 describe('squarer', () => {
 
     it('returns squares greater than 24 of odd numbers', () => {
-        const input = [2, 3, 9, 12, 8, 5, 7];
-        assert.deepEqual(squarer(input), [81, 25, 49]);
-    });
-    
-    it('handles negative numbers in input', () => {
-        const input = [-2, -3, -9, -12, -8, -5, -7];
+        const input = [2, 3, -9, 12, 8, 5, 7];
         assert.deepEqual(squarer(input), [81, 25, 49]);
     });
     
