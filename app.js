a8d63a32239087ca7832d59113ba5ab47debb7db
Mark Myers <markalope@gmail.com>
2018-07-09 16:30:31 -0700

    final commit

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+const result = [];
+
 function oddSquares(arr) {
+    const isOdd = x => x % 2 !== 0;
+    
+
     for(let i=0; i < arr.length; i++) {
-        if
+        if arr[i] === isOdd {
+            result.push(result[i])
+        }
     }
     return result;
 }
