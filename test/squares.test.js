a754ab4173e7f4885b8bebe2ca690f5064c829ab
carriecarter <carrie@carrie-carter.com>
2018-07-09 16:23:14 -0700

    pseudo code test try 1

---
+++ test/squares.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const Squares = require('../lib/Squares');
+
+
+describe('squares', () => {
+
+    it('returns only the odd numbers in the array', () => {
+
+    });
+
+    it('squares each odd number', () => {
+
+    });
+
+    it('returns the squared values greater than 24', () => {
+
+    });
+});
